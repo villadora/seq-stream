289d336d50583910b90faa7adc8c91985ada55b0
Wei Gao <jky239@gmail.com>
2014-07-21 22:36:55 +0800

    change to native stream

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-var through = require('through2');
+var Readable = require('stream').Readable;
+var util = require('util');
+var assert = require('assert');
 
-module.exports = function() {
-  var ended = false;
-  var st = through();
+util.inherits(SeqStream, Readable);
 
-  streams = Array.prototype.slice.apply(arguments);
+function SeqStream(streams) {
+  this.streams = util.isArray(streams) ? streams : Array.prototype.slice.apply(arguments);
+  this.idx = 0;
+}
 
-  st.add = function(stream) {
-    if (ended) throw new Error('SeqStream is already ended');
-    streams.push(stream);
-  };
-
-  appendNext();
-
-  return st;
-
-  function appendNext() {
-    var n = streams.shift();
-    if (n) {
-      n.on('data', function(chunk) {
-        st.write(chunk);
-      });
-
-      n.on('end', appendNext);
-    } else {
-      ended = true;
-      st.end();
+SeqStream.prototype._read = function(size) {
+  var self = this, st;
+  
+  while(self.idx < self.streams.length && (st = self.streams[self.idx])) {
+    var data = st.read(size);
+    if(data) {
+      return self.push(data);
+    }else {
+      self.idx++;
     }
   }
+
+  self.push(null);
+};
+
+SeqStream.prototype.apppend = function() {
+  var added = Array.prototype.slice.apply(arguments);
+  Array.prototype.push.apply(this.streams, added);
+};
+
+SeqStream.prototype.insertNext = function() {
+  var insert = Array.prototype.slice.apply(arguments);
+  insert.unshift(self.idx, 0);
+  Array.protootype.splice.apply(this.streams, insert);
+};
+
+
+module.exports = function() {
+  var streams = Array.prototype.slice.apply(arguments);
+  return new SeqStream(streams);
 };
